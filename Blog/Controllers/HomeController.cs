e708034cd91e97439096ff2a03f9309c4a281738
nasko <nasko>
2016-12-11 17:50:52 +0200

    add starting project

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("ListCategories");
+        }
+
+        public ActionResult ListCategories()
+        {
+            using (var database = new BlogDbContext())
+            {
+                var categories = database.Categories.Include(c => c.Articles).OrderBy(c => c.Name).ToList();
+
+                return View(categories);
+            }
+        }
+        public ActionResult ListArticles(int? categoryId)
+        {
+            if (categoryId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (var database = new BlogDbContext())
+            {
+                var articles = database.Articles
+                    .Where(a => a.CategoryId == categoryId)
+                    .Include(a => a.Author)
+                    .Include(a => a.Tags)
+                    .ToList();
+
+                return View(articles);
+            }
+        }
+    }
+}
---
+++ Blog/Models/ArticleViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class ArticleViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        public string AuthorId { get; set; }
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        public string Tags { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+    }
+}
